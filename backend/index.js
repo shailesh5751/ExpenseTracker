19dd11e5101ae05a67c4337405ea3904e0e73f11
shailesh5751 <shaileshmore2004@gmail.com>
2025-08-24 19:22:04 +0530

    Revert "fix cors settings"
    This reverts commit 7fabfe08c3347a79022abf11523192044030a912.

--- backend/index.js
+++ backend/index.js
@@ -67,11 +67,17 @@ const server = new ApolloServer({
 await server.start();
 
 app.use(
-  "/graphql",
-  express.json(),
-  expressMiddleware(server, {
-    context: async ({ req, res }) => buildContext({ req, res }),
-  })
+	"/graphql",
+	cors({
+		origin: "http://localhost:3000",
+		credentials: true,
+	}),
+	express.json(),
+	// expressMiddleware accepts the same arguments:
+	// an Apollo Server instance and optional configuration options
+	expressMiddleware(server, {
+		context: async ({ req, res }) => buildContext({ req, res }),
+	})
 );
 
 // ========== ✅ Serve frontend ==========
